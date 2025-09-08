cbcb0bc0abda7669d74ad632022fc27d524baa0c
Sachin Kumar <sachin.kumar@i2vsys.com>
2025-09-08 10:54:57 +0530

    Dashboard crud WIP

--- chartinglib/Models/DashboardModel.ts
+++ chartinglib/Models/DashboardModel.ts
@@ -1,8 +1,12 @@
+import { DashboardType } from "src/app/modules/dashboard/Models/dashboard.model";
 import { Widget } from "./Widget";
 
 export class dashboard {
   id: string;
-  analyticManagerId: string;
-  DashboardName: string;
+  // analyticManagerId: string;
   widgets: Widget[];
+  priority: number;
+  description: string
+  name: string;
+  Dashboardtype: DashboardType
 }

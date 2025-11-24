594b864a7eae7cef53c5fba2d2473f6ee9cd75f4
shreyai2v <shreya.jaiswal@i2vsys.com>
2025-11-24 11:56:53 +0530

    propertyFiler can now be access for edit in predefined widgets also

--- chartinglib/widget-form/widget-form.component.ts
+++ chartinglib/widget-form/widget-form.component.ts
@@ -74,7 +74,7 @@ import { CommonComponentsComponent, CommonModalComponent, CommonModalData } from
 import { WidgetFormPreviewComponent } from '../widget-form-preview/widget-form-preview.component';
 import { ToastrService } from 'ngx-toastr';
 import { Property } from 'Analytic/ClientApp/src/app/Models/property.model';
-import { EventPropertyType } from 'Analytic/ClientApp/src/app/Models/eventPropertyType.model';
+import { EventPropertyType, Operators } from 'Analytic/ClientApp/src/app/Models/eventPropertyType.model';
 import { VideoSourceClientManager } from 'Analytic/ClientApp/src/app/Managers/VideoSourceClientManager';
 import { AnalyticEventModel } from 'Analytic/ClientApp/src/app/Models/analyticEvent.Model';
 import { AnalyticService } from 'Analytic/ClientApp/src/app/services/analytic.service';
@@ -1452,13 +1452,16 @@ export class WidgetFormComponent implements OnInit {
     this.allShowablePropertiesName = [];
 
     if (fieldNames && fieldNames.length > 0) {
-      fieldNames.forEach((fieldName: IWidgetFieldNameConfig) => {
+      fieldNames.forEach((field: IWidgetFieldNameConfig) => {
 
+        const finalName = field.applyAggregation
+          ? field.columnName    
+          : field.name;
         this.allShowablePropertiesName.push({
-          name: fieldName.name,
-          displayName: fieldName.columnName,
+          name: finalName,
+          displayName: finalName,
           isMultiValued: false,
-          isLabel: fieldName.type === EventPropertyType.String,
+          isLabel: field.type === EventPropertyType.String,
         });
       });
     }
@@ -1502,6 +1505,17 @@ export class WidgetFormComponent implements OnInit {
     this.recommendedWidgets = availableWidgets;
   }
 
+  operatorToString(operator: string | number): string {
+      if (typeof operator === 'string') {
+        return operator;
+      }
+      return Operators[operator];
+    }
+  
+  stringToOperator(value: string): number {
+    return Operators[value as keyof typeof Operators];
+  }
+    
   onShowablePropertiesChange(event: any): void {
     const selectedProps = event.value;
 
@@ -1510,12 +1524,14 @@ export class WidgetFormComponent implements OnInit {
     showablePropertiesArray.clear();
 
     selectedProps.forEach((prop) => {
-      const isEntityProp = this.entityProperties.find(entityProp => entityProp.name.toLowerCase() == prop.toLowerCase());
+      const possibleBase = this.entityProperties.find(p =>
+        prop.toLowerCase().startsWith(p.name.toLowerCase())
+      );
 
-      if (isEntityProp) {
+      if (possibleBase) {
         showablePropertiesArray.push(this.createShowablePropertyFormGroup({
-          name: isEntityProp.name,
-          displayName: isEntityProp.columnName,
+          name: prop,
+          displayName: prop,
           isLabel: false,
           isMultiValued: false
         }));

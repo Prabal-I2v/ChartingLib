db211f94859f63cfb83f9c57eaa22e9429bcf687
shreyai2v <shreya.jaiswal@i2vsys.com>
2025-11-27 18:08:27 +0530

    corrected logic for predefined and custom widgit for edit

--- chartinglib/widget-form/widget-form.component.ts
+++ chartinglib/widget-form/widget-form.component.ts
@@ -74,7 +74,7 @@ import { CommonComponentsComponent, CommonModalComponent, CommonModalData } from
 import { WidgetFormPreviewComponent } from '../widget-form-preview/widget-form-preview.component';
 import { ToastrService } from 'ngx-toastr';
 import { Property } from 'Analytic/ClientApp/src/app/Models/property.model';
-import { EventPropertyType } from 'Analytic/ClientApp/src/app/Models/eventPropertyType.model';
+import { EventPropertyType, Operators } from 'Analytic/ClientApp/src/app/Models/eventPropertyType.model';
 import { VideoSourceClientManager } from 'Analytic/ClientApp/src/app/Managers/VideoSourceClientManager';
 import { AnalyticEventModel } from 'Analytic/ClientApp/src/app/Models/analyticEvent.Model';
 import { AnalyticService } from 'Analytic/ClientApp/src/app/services/analytic.service';
@@ -1452,13 +1452,16 @@ export class WidgetFormComponent implements OnInit {
     this.allShowablePropertiesName = [];
 
     if (fieldNames && fieldNames.length > 0) {
-      fieldNames.forEach((fieldName: IWidgetFieldNameConfig) => {
+      fieldNames.forEach((field: IWidgetFieldNameConfig) => {
 
+        const finalName = field.applyAggregation
+          ? field.columnName    
+          : field.name;
         this.allShowablePropertiesName.push({
-          name: fieldName.name,
-          displayName: fieldName.columnName,
+          name: finalName,
+          displayName: finalName,
           isMultiValued: false,
-          isLabel: fieldName.type === EventPropertyType.String,
+          isLabel: field.type === EventPropertyType.String,
         });
       });
     }
@@ -1502,6 +1505,16 @@ export class WidgetFormComponent implements OnInit {
     this.recommendedWidgets = availableWidgets;
   }
 
+  operatorToString(operator: string | number): string {
+    if (typeof operator === 'string') {
+      return operator;
+    }
+    return Operators[operator];
+  }
+
+stringToOperator(value: string): number {
+  return Operators[value as keyof typeof Operators];
+}
   onShowablePropertiesChange(event: any): void {
     const selectedProps = event.value;
 
@@ -1510,12 +1523,14 @@ export class WidgetFormComponent implements OnInit {
     showablePropertiesArray.clear();
 
     selectedProps.forEach((prop) => {
-      const isEntityProp = this.entityProperties.find(entityProp => entityProp.name.toLowerCase() == prop.toLowerCase());
+      const possibleBase = this.entityProperties.find(p =>
+        prop.toLowerCase().startsWith(p.name.toLowerCase())
+      );
 
-      if (isEntityProp) {
+      if (possibleBase) {
         showablePropertiesArray.push(this.createShowablePropertyFormGroup({
-          name: isEntityProp.name,
-          displayName: isEntityProp.columnName,
+          name: prop,
+          displayName: prop,
           isLabel: false,
           isMultiValued: false
         }));
@@ -1687,12 +1702,18 @@ export class WidgetFormComponent implements OnInit {
   createFilterPropertyObject(propertyName: string, name: string, type: EventPropertyType, options: any): void {
     const operators = this.getOpertorsByType(type);
     const processedOptions = this.setDefaultValuesByType(options, type);
-
-    const object: any = {
+    let object: any = {};
+    if(type !== EventPropertyType.Guid) {
+    object = {
       name: name,
       type: EventPropertyType[type],
       operators: operators
-    };
+    };} else {
+      object = {
+        name: name,
+        type: "String",
+        operators: operators}
+    }
 
     if (processedOptions !== null) {
       object.options = processedOptions;
@@ -3248,8 +3269,17 @@ export class WidgetFormComponent implements OnInit {
     return this.formMode === Enum_WidgetFormMode.predefined;
   }
 
-  isPredefinedAndConfigurable(): boolean {
-  return this.finalWidget.isWidgetPredefinedAndConfigurable === true;
+  isConfigurable(): boolean {
+    if(this.finalWidget?.isWidgetPredefinedAndConfigurable){
+      return this.finalWidget.isWidgetPredefinedAndConfigurable === true;
+    }else{
+      return true;
+    }
+  }
+  isCopied(): boolean {
+    if(this.finalWidget?.isCopied){
+      return this.finalWidget.isCopied === true;
+    }
   }
   // Update submit button text based on mode
   getSubmitButtonText(): string {
